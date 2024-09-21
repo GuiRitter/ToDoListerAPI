54ee749ebb71500049f1e098f5a60af581a2ca7b
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2024-09-21 17:17:59 -0300

    changed to yarn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import 'dotenv/config'; // always first import
+import '@babel/polyfill'; // always first import
+import 'dotenv/config'; // always second import
 import express from 'express';
-import 'babel-polyfill';
 import cors from 'cors';
 
 import entryRoute from './app/route/entryRoute';
